6ec9d52cebf230890adbf2eb830c9dc5158d7e19
Paras Gherwada <parasgherwada41@gmail.com>
2021-10-28 12:32:19 +0530

    Code Prettified

--- Bullet.java
+++ Bullet.java
@@ -1,21 +1,25 @@
-class Bullet extends ShipModel
-{
-    Bullet(double x,double y) throws Exception
-    {
-        super(x+8,y+8,"images/bullet/2.png",0);
+class Bullet extends ShipModel {
+    Bullet(double x, double y) throws Exception {
+        super(
+            x + 8,
+            y + 8,
+            "images/bullet/2.png",
+            0
+        );
     }
-    void move()
-    {
+    void move() {
         accelerate(0.12);
         super.move();
     }
 
-    boolean isOutOfBounds()
-    {
-        double Xpos=this.getObject().getBoundsInParent().getMaxX();
-        double Ypos=this.getObject().getBoundsInParent().getMaxY();
+    boolean isOutOfBounds() {
+        double Xpos = this.getObject().getBoundsInParent().getMaxX();
+        double Ypos = this.getObject().getBoundsInParent().getMaxY();
+        return (Xpos < 0 || Xpos > GameLoader.WIDTH || Ypos < 0 || Ypos > GameLoader.HEIGHT);
+        /* 
         if(Xpos<0 || Xpos>GameLoader.WIDTH || Ypos<0 || Ypos>GameLoader.HEIGHT)
             return true;
         return false;
+        */
     }
 }
--- PlayerShip.java
+++ PlayerShip.java
@@ -1,22 +1,15 @@
-class PlayerShip extends ShipModel
-{
-    PlayerShip(double x,double y) throws Exception
-    {
-        super(x,y,"images/player/0.png",39);
+class PlayerShip extends ShipModel {
+    PlayerShip(double x, double y) throws Exception {
+        super(x, y, "images/player/0.png", 39);
     }
     
-    void move()
-    {
+    void move() {
         super.move();
-        double Xpos=this.getObject().getTranslateX();
-        double Ypos=this.getObject().getTranslateY();
-        if(Xpos<0)
-            resetPosition(0,Ypos);
-        if(Xpos>760)
-            resetPosition(760,Ypos);
-        if(Ypos<0)
-            resetPosition(Xpos,0);
-        if(Ypos>560)
-            resetPosition(Xpos,560);
+        double Xpos = this.getObject().getTranslateX();
+        double Ypos = this.getObject().getTranslateY();
+        if(Xpos<0) resetPosition(0,Ypos);
+        if(Xpos>760) resetPosition(760,Ypos);
+        if(Ypos<0) resetPosition(Xpos,0);
+        if(Ypos>560) resetPosition(Xpos,560);
     }
 }
